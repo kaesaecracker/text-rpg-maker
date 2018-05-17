072a5c26f0e497b1f1d9d0379ab9e0e3e6501581
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-05-17 14:57:11 +0200

    Init

---
+++ TextRpgMaker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextRpgMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
