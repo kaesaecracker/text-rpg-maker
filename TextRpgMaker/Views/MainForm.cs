c6cb888bb3cc3489af82b68c00d935c7404be754
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-05-30 08:50:13 +0200

    Merge MainForm.cs with MainForm.Menu.cs

--- TextRpgMaker/Views/MainForm.cs
+++ TextRpgMaker/Views/MainForm.cs
@@ -51,11 +51,46 @@ namespace TextRpgMaker.Views
             this.Content = layout;
         }
         
+        private static MenuBar InitializeMenu() => new MenuBar
+        {
+            Items =
+            {
+                new ButtonMenuItem
+                {
+                    Text = "Open Game",
+                    Command = new Command(UnimplementedClick)
+                },
+                new ButtonMenuItem
+                {
+                    Text = "Load / Save",
+                    Command = new Command(UnimplementedClick)
+                }
+            },
+
+            AboutItem = new ButtonMenuItem
+            {
+                Text = "About",
+                Command = new Command(UnimplementedClick)
+            },
+
+            HelpItems =
+            {
+                new ButtonMenuItem
+                {
+                    Text = "Game Help",
+                    Command = new Command(UnimplementedClick)
+                },
+                new ButtonMenuItem
+                {
+                    Text = "Engine Help",
+                    Command = new Command(UnimplementedClick)
+                }
+            }
+        };
+        
         private void ExitClick(object sender, EventArgs e) => Application.Instance.Quit();
 
         internal static void UnimplementedClick(object sender, EventArgs e)
             => MessageBox.Show("Not implemented yet");
-        
-        
     }
 }
