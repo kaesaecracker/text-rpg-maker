5c9a3f1be2cc97f2ab3fcb486dbcf9fef731dfd3
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-06-06 09:06:28 +0200

    Move Annotations, Exceptions and State

---
+++ TextRpgMaker/AppState.cs
@@ -0,0 +1,15 @@
+﻿namespace TextRpgMaker.Models
+{
+    public class State
+    {
+        public string PathToProject { get; private set; }
+
+        public Project LoadedProject { get; private set; }
+
+        public State(string pathToProject)
+        {
+            this.PathToProject = pathToProject;
+            this.LoadedProject = new Project(this.PathToProject);
+        }
+    }
+}
--- TextRpgMaker/Models/Project.Helpers.cs
+++ TextRpgMaker/Models/Project.Helpers.cs
@@ -75,37 +75,4 @@ namespace TextRpgMaker.Models
             private const string ScenesFolder = "scenes";
         }
     }
-
-    public class LoadFailedException : Exception
-    {
-        public static LoadFailedException FileMissing(string fileInProject, string triedPath) =>
-            new LoadFailedException(
-                $"The required project file {fileInProject} is missing!\n" +
-                $" Expected it at {triedPath}"
-            );
-
-        public static LoadFailedException DuplicateIds(IEnumerable<IGrouping<string, Element>> duplicates) =>
-            new LoadFailedException(
-                "The project contains duplicate element ids, which is not allowed. The duplicate ids are: " +
-                $"{string.Join(", ", duplicates.Select(d => d.Key))}"
-            );
-
-        public static LoadFailedException BaseElementNotFound(Element element, InvalidOperationException ex) =>
-            new LoadFailedException(
-                $"The item id {element.Id} is based on {element.BasedOnId} which could not be found",
-                ex
-            );
-
-        public static LoadFailedException BaseElementHasDifferentType(Element baseElem, Element targetElem) =>
-            new LoadFailedException(
-                $"The item id '{targetElem.Id}' is based on '{baseElem.Id}', but the types are different. (" +
-                $"'{targetElem.Id}' has type '{targetElem.GetType().Name}', " +
-                $"'{baseElem.Id}' is of type '{baseElem.GetType().Name}')"
-            );
-
-        public LoadFailedException(string message, Exception innerException = null)
-            : base(message, innerException)
-        {
-        }
-    }
 }
