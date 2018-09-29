76d06aedd228ebf8d86048b2586e3231580310b9
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-09-29 14:39:30 +0200

    Only show choices that meet the requirements in dialogs

--- TextRpgMaker/ProjectModels/Inventory.cs
+++ TextRpgMaker/ProjectModels/Inventory.cs
@@ -1,17 +1,64 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Gtk;
 using TextRpgMaker.Helpers;
 using YamlDotNet.Serialization;
 
 namespace TextRpgMaker.ProjectModels
 {
     [DocumentedType]
+    // TODO make List<> invisible from outside (probably big refactor)
     public class Inventory : List<ItemGrouping>
     {
         public override string ToString()
         {
             var names = this.Select(ig => AppState.Project.ById<BasicElement>(ig.ItemId).Name);
             return $"[{names.Aggregate((curr, name) => $"{curr}, {name}")}]";
+        }
+
+        private ItemGrouping GetItemGrouping(string id)
+        {
+            foreach (var ig in this)
+            {
+                if (ig.ItemId == id) return ig;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Checks whether the inventory contains the item type, and whether it contains at least the amount specified
+        /// </summary>
+        public bool HasItem(ItemGrouping groupingToFind)
+        {
+            var ig = this.GetItemGrouping(groupingToFind.ItemId);
+            return ig != null && ig.Count >= groupingToFind.Count;
+        }
+
+        /// <summary>
+        /// Add an item to the inventory
+        /// </summary>
+        public void AddItem(ItemGrouping item)
+        {
+            var ig = this.GetItemGrouping(item.ItemId);
+            if (ig != null) ig.Count += item.Count;
+            else this.Add(item);
+        }
+
+        /// <summary>
+        /// Removes the specified item (count) from the inventory.
+        /// </summary>
+        /// <returns>false if inventory does not contain enough to remove</returns>
+        public bool RemoveItem(ItemGrouping item)
+        {
+            if (!this.HasItem(item)) return false;
+            
+            var ig = this.GetItemGrouping(item.ItemId);
+            ig.Count -= item.Count;
+
+            if (ig.Count <= 0) this.Remove(item);
+            return true;
         }
     }
 
@@ -23,7 +70,7 @@ namespace TextRpgMaker.ProjectModels
             // empty contructor for serialization
         }
 
-        public ItemGrouping(string itemId, int count)
+        public ItemGrouping(string itemId, uint count)
         {
             this.ItemId = itemId;
             this.Count = count;
@@ -35,6 +82,6 @@ namespace TextRpgMaker.ProjectModels
 
         [YamlMember(Alias = "count")]
         [YamlProperties(false, 1)]
-        public int Count { get; set; }
+        public uint Count { get; set; }
     }
 }
